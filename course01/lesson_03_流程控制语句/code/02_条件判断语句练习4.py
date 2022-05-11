4ba9d5d5b34703d0fe4991bd700d431a9ec0e317
jermyHui <505577593@qq.com>
2022-05-11 22:46:29 +0800

    add 练习4、练习5

--- course01/lesson_03_流程控制语句/code/02_条件判断语句练习4.py
+++ course01/lesson_03_流程控制语句/code/02_条件判断语句练习4.py
@@ -1,4 +1,13 @@
-# 获取小明的期末成绩
-score = int(input("请输入期末成绩"))
-
-#
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 获取期末成绩
+score = int(input("请输入期末成绩："))
+# 判断分数不同给与不同奖励
+if score == 100:
+    print("奖励一辆BMW")
+elif 80 <= score <= 99:
+    print("奖励一台iphone")
+elif 60 <= score <= 79:
+    print("奖励一本参考书")
+else:
+    print("成绩不合格，没有奖励")
---
+++ course01/lesson_03_流程控制语句/code/03_条件判断语句练习5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 获取男生身高、财产和帅气值
+height = int(input("请输入身高："))
+property = int(input("请输入财富值："))
+looks = int(input("请输入帅气度："))
+# 根据条件不同判断是否嫁
+if height > 180 and property > 1000 and looks > 500:
+    print("我一定要嫁给他")
+elif height > 180 or property > 1000 or looks > 500:
+    print("嫁吧，比上不足，比下有余。")
+elif height <= 180 and property <= 1000 and looks <= 500:
+    print("不嫁！")
