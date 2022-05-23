1a1a64ca73f639d36b34a5f3bf74b0f0bed4056e
jermyHui <505577593@qq.com>
2022-05-23 16:17:41 +0800

    序列章节新增字典练习12

---
+++ course01/lesson_04_序列/12_字典.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 创建一个字典
+user_info = {
+    "name" : "chenchen",
+    "age" : 18,
+    "sex" : "女",
+    "地址" : "beijing"
+}
+# 打印用户信息
+print("我叫",user_info["name"],"今年",user_info["age"],"岁","性别",user_info["sex"],"住在",user_info["地址"])
+
