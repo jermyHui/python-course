7ea89f2fc672b766a6bb192c387810f2df3343e9
jermyHui <505577593@qq.com>
2022-08-25 11:57:14 +0800

    random模块练习

---
+++ python_detail/code/29_随机验证码.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.8
+# -*- coding: UTF-8 -*-
+# 随机产生6位数字和字母组合的验证码
+import os
+import random
+# 定义随机选择的字母变量
+# random_chr = chr(random.randint(65,90))
+# 定义随机选择数字的变量
+# random_num = str(random.randint(0,9))
+#
+# def random_verification_code(lenth=4):
+#     # 定义一个存储验证码的数组
+#     verification_code = ''
+#     for i in range(lenth):
+#         random_value = random.choice([random_chr,random_num])
+#         verification_code += random_value
+#     return verification_code
+# res = random_verification_code(8)
+# print(res)
+
+
+'''
+@version:3.8
+@author:chenchen
+@file:29_随机验证码.py
+@time:2022/8/25 11:10
+'''
