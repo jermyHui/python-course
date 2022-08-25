7ec300f035b983decd021b50aedf0ddc11cb60d5
jermyHui <505577593@qq.com>
2022-08-25 11:09:53 +0800

    模块导入练习、包的概念

---
+++ python_detail/code/28_模块的导入.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.8
+# -*- coding: UTF-8 -*-
+# import sys
+# sys.path.append(r'D:\github\python_detail')
+# import package_test
+# print(sys.path)
+# print(sys.modules)
+# print(__file__)
+#
+# import time
+# print(time.time())
+# print(time.localtime().tm_mday+7)
+# print(time.strftime('%Y-%m-%d %H:%M:%S'))
+import random
+
+print(random.random())
+'''
+@version:3.8
+@author:chenchen
+@file:28_模块的导入.py
+@time:2022/8/23 21:50
+'''
---
+++ python_detail/package_test/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.8
+# -*- coding: UTF-8 -*-
+
+'''
+@version:3.8
+@author:chenchen
+@file:__init__.py.py
+@time:2022/8/24 14:43
+'''
